5ec9cef560528dbe3bcd5c6eddda31b3b9a330f3
imlikefire <robertungureanu551@yahoo.com>
2017-05-11 21:03:26 +0300

    Start Project

---
+++ Angular/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class TwPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
